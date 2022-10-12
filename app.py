932b3c58c3fb8b36ed39fe24f3511fad75337f75
Felipe Cruz <felipe.oliveiracrz@outlook.com>
2022-10-11 23:43:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
 from flask import Flask
 import requests
 import json
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def all():
-    x = os.environ.get("REQUEST_URL")
     request = requests.get("https://www.diretodostrens.com.br/api/status")
     data = json.loads(request.content)
     return data
 
 @app.route("/status/<int:status_id>", methods=["GET"])
 def status(status_id):
-    x = os.environ.get("REQUEST_URL")
     request = requests.get("https://www.diretodostrens.com.br/api/status")
     data = json.loads(request.content)
     data = [x for x in data if x['codigo'] == status_id]
