87c39b8fc8838b5d554f8ea79c219879afb1c7e1
Felipe Cruz <felipe.oliveiracrz@outlook.com>
2022-10-11 21:30:24 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 from app import app
 import unittest
+import random
 
 class Test(unittest.TestCase):
     def setUp(self):
         # cria uma instância do unittest, precisa do nome "setUp"
         self.app = app.test_client()
 
+        x = random.randint(1,10)
+        
         # envia uma requisicao GET para a URL
-        self.result = self.app.get('/status')
+        self.result = self.app.get('/status/x')
 
     def test_requisicao(self):
         # compara o status da requisicao (precisa ser igual a 200)
